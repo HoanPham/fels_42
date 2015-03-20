c7bcea67307144404cde55db430ddb51d8feeb2f
framgia <redriver91@gmail.com>
2015-03-20 11:02:10 +0700

    Implement users features

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users
   get 'static_pages/home'
 
@@ -15,7 +16,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :users
 
   # Example resource route with options:
   #   resources :products do
